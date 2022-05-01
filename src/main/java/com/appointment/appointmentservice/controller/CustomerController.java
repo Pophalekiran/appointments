a8d8ce5aa0c6874b4034525aa8c1a66b4ec05e16
Pophalekiran <pophale.kiran@gmail.com>
2022-05-01 17:53:26 +0530

    first checkin

---
+++ src/main/java/com/appointment/appointmentservice/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.appointment.appointmentservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appointment.appointmentservice.model.CustomerVo;
+import com.appointment.appointmentservice.service.ICustomerService;
+
+@RestController
+public class CustomerController {
+
+	
+	@Autowired
+	ICustomerService customerSrevice;
+	
+	
+	@GetMapping("/v1/customers")
+	public List<CustomerVo> retrieveCustomers() {
+		
+		return customerSrevice.retrieveCustomer();
+		
+	}
+	
+}
